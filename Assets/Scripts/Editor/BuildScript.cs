c3c55c659e93a86c931c8649ca2154cdc6ecd4ed
PaulIm <woosh3@yahoo.com>
2024-08-07 18:02:39 -0700

    up2

--- Assets/Scripts/Editor/BuildScript.cs
+++ Assets/Scripts/Editor/BuildScript.cs
@@ -1,11 +1,14 @@
 using UnityEditor;
+using UnityEngine;
 
 public class BuildScript
 {
     public static void BuildAndroid()
     {
+        Debug.Log("BuildScript.BuildAndroid is called");
         string[] scenes = { "Assets/SampleScene.unity" };
         string outputPath = "Builds/WindowsBuild.exe";
         BuildPipeline.BuildPlayer(scenes, outputPath, BuildTarget.StandaloneWindows64, BuildOptions.None);
+        Debug.Log("BuildScript.BuildAndroid is exiting");
     }
 }
